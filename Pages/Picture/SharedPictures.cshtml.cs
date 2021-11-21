089e8883b9533e40e104e8f84bb1f7cd13ed0384
Chane <vanderbergchane@gmail.com>
2021-11-21 10:19:44 +0200

    Created a shared pictures page

---
+++ Pages/Picture/SharedPictures.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PixCollab.Data;
+using PixCollab.Models;
+
+namespace PixCollab.Pages.Picture
+{
+    public class SharedPicturesModel : PageModel
+    {
+        private readonly PixCollab.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SharedPicturesModel(PixCollab.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IList<Models.Picture> Picture { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var userid = _userManager.GetUserAsync(User).Result.Id;
+
+            string qry = string.Format(@"SELECT p.* FROM dbo.Picture p
+left outer join dbo.PictureAccess pa
+on pa.PhotoId = p.ID
+where pa.UserId = '{0}'", userid);
+
+            Picture = await _context.Picture
+                .FromSqlRaw(qry).Include(x => x.Owner).ToListAsync();
+        }
+    }
+}
